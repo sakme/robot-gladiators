6efa103d29028d52fd4fa8dc5c5b12d35a77d4ae
sakme <alan@akme.us>
2021-11-09 14:28:05 -0500

    plaing with variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,18 @@
-window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("What is your robot's name?");
+var name = "Alan";
+
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message.");
+// this will do math and log 20
+console.log(10+10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+console.log(name);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("Thie fight has begun!");
+}
+
+// fight();
